4f733c3bf95699f77737b81961a0033772f7080a
Bartek Pacia <barpac02@gmail.com>
2020-07-13 16:09:02 +0200

    change example fixture, refactor code

--- src/actions/assign.ts
+++ src/actions/assign.ts
@@ -2,16 +2,47 @@ import { Context } from "probot"
 import { different } from "../utils"
 
 export = async function assign(context: Context) {
-  // Get PR number and current PR reviewers
   const PRnumber = context.payload.number
+  const owner = context.payload.pull_request.head.user.login
+  const repo = context.payload.pull_request.head.repo.name
+
+  context.log.info("PR number:", PRnumber, "owner:", owner, "repo:", repo)
+
   const oldReviewers: string[] = context.payload.pull_request.requested_reviewers.map(
     (reviewer: { login: string }) => reviewer
   )
 
-  context.log.info("PR number:", PRnumber)
+  let newReviewers: string[] = []
+  try {
+    newReviewers = await reviewersOfPR(context, PRnumber, owner, repo)
+  } catch (err) {
+    context.log.error(err, "Error while calling reviewersOfPR function")
+    return
+  }
 
-  const owner = context.payload.pull_request.head.user.login
-  const repo = context.payload.pull_request.head.repo.name
+  // Check if old and new reviewers differ. Otherwise we can skip an API call
+  if (different(oldReviewers, newReviewers)) {
+    // Log reviewers replacement
+    context.log.info("Old reviewers:", oldReviewers)
+    context.log.info("New reviewers:", newReviewers)
+
+    const params = context.repo({ pull_number: PRnumber, reviewers: newReviewers })
+    await context.github.pulls.createReviewRequest(params)
+  } else {
+    context.log.info("No reviewers changed")
+  }
+}
+
+/**
+ * @return List of maintainers of the files that were modified in `PRnumber`
+ */
+async function reviewersOfPR(
+  context: Context,
+  PRnumber: number,
+  owner: string,
+  repo: string
+): Promise<string[]> {
+  const reviewers: string[] = []
 
   const codeownersResponse = await context.github.repos.getContents({
     owner: owner,
@@ -35,41 +66,6 @@ export = async function assign(context: Context) {
 
   const codeowners = Buffer.from(data.content, "base64").toString()
   context.log.info(codeowners)
-
-  let newReviewers: string[] = []
-  try {
-    newReviewers = reviewersOfPR(PRnumber)
-  } catch (err) {
-    context.log.error(err, "Error while calling reviewersOfPR function")
-    return
-  }
-
-  // Check if old and new reviewers differ. Otherwise we can skip an API call
-  if (different(oldReviewers, newReviewers)) {
-    // Log reviewers replacement
-    context.log.info("Old reviewers:", oldReviewers)
-    context.log.info("New reviewers:", newReviewers)
-
-    const params = context.repo({ pull_number: PRnumber, reviewers: newReviewers })
-    await context.github.pulls.createReviewRequest(params)
-  } else {
-    context.log.info("No reviewers changed")
-  }
-}
-
-/**
- * Executes the zsh script that assign reviewers to the PR of PRNumber.
- * The zsh script assign the reviewers basing on the files that have been changed.
- *
- * @return The list of maintainers of the changed files
- */
-function reviewersOfPR(PRNumber: number): string[] {
-  const reviewers: string[] = []
-
-  // TODO: Parse files that changed in the PR of PRNumber
-  // TODO: Find whether any of the changed file has the owner (defined in CODEOWNERS)
-  // TODO: If there is a match, request a review from the responsible user (a.k.a assign a PR reviewer)
-  // TODO: Write the logic in TypeScript
 
   return reviewers
 }
